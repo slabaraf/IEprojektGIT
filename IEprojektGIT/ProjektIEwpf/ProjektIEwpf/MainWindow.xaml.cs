e51511579177a7669b26fab3292cf860a6b7bde4
Przemek Wojtarowicz <infanymousss@gmail.com>
2018-04-14 21:38:16 +0200

    baza i checkboxy w listboksie

---
+++ IEprojektGIT/ProjektIEwpf/ProjektIEwpf/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;
+
+namespace ProjektIEwpf
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\projekty\IE2\IEprojektGIT\Database4.accdb");
+            con.Open();
+            string queryString = "SELECT * FROM Produkty";
+            OleDbCommand cmd = new OleDbCommand(queryString, con);
+            OleDbDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                RekomendowaneLstbx.Items.Add(reader.GetString(1)+ ", " + reader.GetString(2));
+            }
+
+            
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ObejrzyjBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (RekomendowaneLstbx.SelectedValue!=null)
+            {
+                Window obejrzyj = new Window();
+            }
+            
+            
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
