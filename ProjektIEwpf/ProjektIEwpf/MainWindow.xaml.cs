d9d6cdfd7c66a79574d42e03c55afb53f36160c4
Przemek Wojtarowicz <infanymousss@gmail.com>
2018-04-14 21:39:14 +0200

    baza + jakies checkboxy w listboksie

--- ProjektIEwpf/ProjektIEwpf/MainWindow.xaml.cs
+++ ProjektIEwpf/ProjektIEwpf/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.OleDb;
 
 namespace ProjektIEwpf
 {
@@ -23,6 +24,19 @@ namespace ProjektIEwpf
         public MainWindow()
         {
             InitializeComponent();
+            OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\projekty\IE2\IEprojektGIT\Database4.accdb");
+            con.Open();
+            string queryString = "SELECT * FROM Produkty";
+            OleDbCommand cmd = new OleDbCommand(queryString, con);
+            OleDbDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                RekomendowaneLstbx.Items.Add(reader.GetString(1)+ ", " + reader.GetString(2));
+            }
+
+            
+
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -36,6 +50,21 @@ namespace ProjektIEwpf
         }
 
         private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ObejrzyjBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (RekomendowaneLstbx.SelectedValue!=null)
+            {
+                Window obejrzyj = new Window();
+            }
+            
+            
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
